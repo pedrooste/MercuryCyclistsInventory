2a551749589b0591e325db3dfeb26b7fdac13e8e
Zhifa Qiu <zhifaq@Zhifas-MacBook-Air.local>
2022-09-15 15:51:09 +1000

    Adding new service to reuse some of the code, adding supplier validation for add / update part methods

--- src/main/java/com/mercuryCyclists/Inventory/controller/PartController.java
+++ src/main/java/com/mercuryCyclists/Inventory/controller/PartController.java
@@ -2,37 +2,32 @@ package com.mercuryCyclists.Inventory.controller;
 
 import com.mercuryCyclists.Inventory.entity.Part;
 import com.mercuryCyclists.Inventory.service.PartService;
+import com.mercuryCyclists.Inventory.service.RestfulService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/part")
 public class PartController {
     private PartService partService;
-    private static final String GETSUPLLIERAPI = "http://localhost:8080/api/v1/supplier/{supplierId}";
-    private static RestTemplate restTemplate = new RestTemplate();
+    private RestfulService restfulService;
 
-    public PartController(PartService partService) {
+    public PartController(PartService partService, RestfulService restfulService) {
         this.partService = partService;
-
+        this.restfulService = restfulService;
     }
 
     /**
      * Find supplier with given part id
+     *
      * @param partId
      */
     @GetMapping("{partId}/supplier")
     public ResponseEntity<String> findSupplier(@PathVariable Long partId) {
         Part part = partService.findPartByPartId(partId);
         if (part == null) return new ResponseEntity<>("Invalid part ID", HttpStatus.FAILED_DEPENDENCY);
-        Map<String, Long> param = new HashMap<>();
-        param.put("supplierId", part.getSupplierId());
-        String result = restTemplate.getForObject(GETSUPLLIERAPI, String.class, param);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return restfulService.getSupplier(part.getSupplierId());
     }
 }
